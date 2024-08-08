443e33806a87c57db713eb1ea5cd81c3b16d53ef
Tong Yin <yin.tong@mitigenomics.com>
2024-08-07 19:15:40 -0700

    添加缩放功能

--- adbutils.py
+++ adbutils.py
@@ -3,9 +3,11 @@ import time
 
 from adb_shell.adb_device import AdbDeviceTcp
 
+
 n = 6  # n为模拟器编号，初始为0
 
-device = AdbDeviceTcp('127.0.0.1', 16384 + 32 * n)
+# device = AdbDeviceTcp('127.0.0.1', 16384 + 32 * n)
+device = AdbDeviceTcp('127.0.0.1', 5555)
 device.connect()
 
 
@@ -15,84 +17,35 @@ def get_screenshot():
     time.sleep(1)  # 图片传输可能会有延迟
 
 
-# 缩小屏幕(半有效，但有多线程超时问题)
-def shrink_screen():
-    for i in range(0, 3):
-        threads = []
-        script_thread_up = threading.Thread(target=up)
-        script_thread_down = threading.Thread(target=down)
-        threads.append(script_thread_up)
-        threads.append(script_thread_down)
+def swipe(start_x, start_y, end_x, end_y, duration):
+    device.shell(f"input touchscreen swipe {start_x} {start_y} {end_x} {end_y} 200")
 
-        for t in threads:
-            t.start()
+def zoom_out():
+    device.shell("sh /sdcard/zoom_out.sh")   # try running the script to pinchout    
 
-        time.sleep(3)
-
-        for t in threads:
-            t.join()
-
-        time.sleep(3)
-
+def zoom_in():
+    device.shell("sh /sdcard/zoom_in.sh")   # try running the script to zoomin    
 
 def up():
     device.shell("input swipe {} {} {} {}".format(1000, 200, 1000, 500))
 
-
 def down():
     device.shell("input swipe {} {} {} {}".format(1000, 800, 1000, 500))
-
 
 def click(x, y):
     device.shell("input tap {} {}".format(x, y))
 
 
-# 缩小屏幕，无效果，可能哪里写错了
-def send_event():
-    device.shell("sendevent /dev/input/event4 3 57 0")
-    device.shell("sendevent /dev/input/event4 3 53 300")
-    device.shell("sendevent /dev/input/event4 3 54 800")
-    device.shell("sendevent /dev/input/event4 0 0 0")
-
-    device.shell("sendevent /dev/input/event4 3 57 1")
-    device.shell("sendevent /dev/input/event4 3 53 800")
-    device.shell("sendevent /dev/input/event4 3 54 800")
-    device.shell("sendevent /dev/input/event4 0 0 0")
-
-    device.shell("sendevent /dev/input/event4 3 53 350")
-    device.shell("sendevent /dev/input/event4 3 54 800")
-
-    device.shell("sendevent /dev/input/event4 3 57 0")
-    device.shell("sendevent /dev/input/event4 3 53 750")
-    device.shell("sendevent /dev/input/event4 3 54 800")
-    device.shell("sendevent /dev/input/event4 3 57 1")
-    device.shell("sendevent /dev/input/event4 0 0 0")
-
-    device.shell("sendevent /dev/input/event4 3 53 400")
-    device.shell("sendevent /dev/input/event4 3 54 800")
-    device.shell("sendevent /dev/input/event4 3 57 0")
-    device.shell("sendevent /dev/input/event4 3 53 700")
-    device.shell("sendevent /dev/input/event4 3 54 800")
-    device.shell("sendevent /dev/input/event4 3 57 1")
-    device.shell("sendevent /dev/input/event4 0 0 0")
-
-    device.shell("sendevent /dev/input/event4 3 57 4294967295")
-    device.shell("sendevent /dev/input/event4 0 0 0")
-
-    device.shell("sendevent /dev/input/event4 3 57 4294967295")
-    device.shell("sendevent /dev/input/event4 0 0 0")
-
-
-# 缩小屏幕，无效果，阻塞执行
-def move_screen():
-    device.shell("input touchscreen swipe 1000 200 1000 500")
-    device.shell("input touchscreen swipe 1000 800 1000 500")
-
-
+# 把缩小和放大的脚本放到模拟器上
+def send_scripts():
+    device.push("zoom_in.sh","/sdcard/zoom_in.sh")
+    device.push("zoom_out.sh","/sdcard/zoom_out.sh")
+# 
+send_scripts()
 # get_screenshot(device)
-# shrink_screen()
-# send_event()
 move_screen()
+zoom_in()
+zoom_out()
 # click(1510, 955, 1)
-
 device.close()
+
---
+++ hex2dec.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+# coding: utf-8
+
+import sys
+
+def hex_to_dec(filename):
+    # Command prefix
+    prefix = "sendevent"
+
+    # Create output file, taking everything before the extension
+    outfile = filename.rsplit('.', 1)[0] + ".sh"
+
+    # Open the input and output files
+    with open(filename, "r") as fo, open(outfile, "w") as fw:
+        # Write the script header
+        fw.write("#!/bin/sh\n")
+        #fw.write("echo Running – drawing function\n")
+
+        # Process each line in the input file
+        for inputline in fo:
+            # Find the location of the colon
+            part1len = inputline.find(":")
+
+            # If colon is found, process the line
+            if part1len > -1:
+                # Extract the device path
+                part1 = inputline[:part1len].strip()
+
+                # Split the line into parts and convert hex to decimal
+                part2 = inputline[part1len + 1:].strip().split()
+                if len(part2) >= 3:
+                    try:
+                        num1 = int(part2[0], 16)
+                        num2 = int(part2[1], 16)
+                        num3 = int(part2[2], 16)
+
+                        # Assemble the new command
+                        complete = f"{prefix} {part1} {num1} {num2} {num3}"
+
+                        # Write the command to the output file
+                        fw.write(complete + "\n")
+                    except ValueError:
+                        print(f"Skipping line due to conversion error: {inputline.strip()}")
+                else:
+                    print(f"Skipping line due to insufficient data: {inputline.strip()}")
+
+    # Inform the user
+    print("Processing complete")
+    print(f"File created: {outfile}")
+    print(f"Copy file to the device with: adb push {outfile} /sdcard/{outfile}")
+    print(f"Run the script with: adb shell sh /sdcard/{outfile}")
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python hex_to_dec.py <inputfile>")
+        sys.exit(1)
+
+    hex_to_dec(sys.argv[1])
